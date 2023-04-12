3183afc060795d8191937ef8b74c604fa599e57c
Santanu Dutta <santanu.techie32@gmail.com>
2023-04-12 08:46:25 +0530

    Added image upload functionality for users and places

--- backend/routes/users-routes.js
+++ backend/routes/users-routes.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const { check } = require("express-validator");
+const fileUpload = require("../middleware/file-upload");
 
 // Import controllers
 const usersControllers = require("../controllers/users-controllers");
@@ -10,6 +11,7 @@ router.get("/", usersControllers.getUsers);
 
 router.post(
   "/signup",
+  fileUpload.single("image"),
   [
     check("name").not().isEmpty(),
     check("email").normalizeEmail().isEmail(),
